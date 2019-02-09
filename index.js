78e47f290b755adde0183fbb58902638785cba4e
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-09 21:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   return "YES INDEED!"
  else if (string.toLowerCase() === string)
   return "I can't hear you!"
- else (string === "I love you, Grandma.") 
+ else if (string === "I love you, Grandma.") 
   return "I love you, too."
 }
