b672a784aa727d2a8d7d220da99ad617cc053f8f
Tom MacLean <tom.j.maclean@gmail.com>
2019-02-09 20:55:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
   return "YES INDEED!"
-} if (string.toLowerCase() === string) {
+} else if (string.toLowerCase() === string) {
   return "I can't hear you!"
-} else if (string.toUpperCase() === "I love you, Grandma.") {
+} else (string.toUpperCase() === "I love you, Grandma.") {
   return "I love you, too."
 }
